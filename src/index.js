5004c67ae18fbde1c989620d6ac657d68b08018b
Gentlemanatthemorning <sergey25ka@gmail.com>
2018-10-27 22:44:45 +0200

    Added webpack technology

---
+++ src/index.js
@@ -0,0 +1,41 @@
+console.log(`I've been required by Webpack`);
+import style from "./_scss/main.scss";
+
+
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     var next = document.getElementById('nextPic');
+//     var prev = document.getElementById('prevPic');
+//     var array = [];
+//     var allLi = document.querySelectorAll('.banner__slider li');
+//     var show = 0;
+
+//     allLi[show].classList.add('visible');
+
+//     next.addEventListener('click', () => {
+//         allLi[show].classList.remove('visible');
+//         show++;
+//         (show >= allLi.length) ?
+//             show = 0:null;
+
+//         allLi[show].classList.add('visible');
+//     });
+
+//     next.addEventListener(setInterval( () => {
+//         allLi[show].classList.remove('visible');
+//             show++;
+//             (show >= allLi.length) ?
+//                 show = 0:null;
+    
+//             allLi[show].classList.add('visible');
+//     }, 2000));
+    
+//     prev.addEventListener('click', () => {
+//         allLi[show].classList.remove('visible');
+//         show--;
+//         (show <0) ?
+//             show = allLi.length -1:null;
+
+//         allLi[show].classList.add('visible');
+//     })
+// });
+
